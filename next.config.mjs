04ac77a5367022a7e10fa9e9cd51b2df43105c33
sibulut <sibulut@outlook.com>
2024-12-02 17:00:21 -0500

    Fix next_config_mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -14,11 +14,11 @@ const nextConfig = {
   env: {
     USER_POOL_ID: process.env.USER_POOL_ID,
     USER_POOL_CLIENT_ID: process.env.USER_POOL_CLIENT_ID,
-    IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID
-    DYNAMODB_TABLE_NAME: process.env.DYNAMODB_TABLE_NAME
-    APP_SECRET_ACCESS_KEY: process.env.APP_SECRET_ACCESS_KEY
-    APP_REGION: process.env.APP_REGION
-    APP_ACCESS_KEY_ID: process.env.APP_ACCESS_KEY_ID
+    IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID,
+    DYNAMODB_TABLE_NAME: process.env.DYNAMODB_TABLE_NAME,
+    APP_SECRET_ACCESS_KEY: process.env.APP_SECRET_ACCESS_KEY,
+    APP_REGION: process.env.APP_REGION,
+    APP_ACCESS_KEY_ID: process.env.APP_ACCESS_KEY_ID,
   },
 };
 
